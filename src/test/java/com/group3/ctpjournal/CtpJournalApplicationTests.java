e64c9208593f2af4e31039d11d7e85004551817f
Paul Lacaes <paul.lacaes@ig2i.centralelille.fr>
2021-03-23 11:13:07 +0100

    First commit

---
+++ src/test/java/com/group3/ctpjournal/CtpJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.ctpjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtpJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
